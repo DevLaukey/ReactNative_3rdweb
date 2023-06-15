83d276464515f7e90fcda1e718f2465c7d802ebd
DevLaukey <devlaukeymwaura@gmail.com>
2023-06-15 17:36:00 +0300

    RN_template

--- android/app/src/main/java/com/anonymous/reactnativestarter/MainActivity.java
+++ android/app/src/main/java/com/anonymous/reactnativestarter/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativestarter;
+package com.anonymous.reactnativestarter;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/anonymous/reactnativestarter/MainApplication.java
+++ android/app/src/main/java/com/anonymous/reactnativestarter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativestarter;
+package com.anonymous.reactnativestarter;
 
 import android.app.Application;
 import android.content.res.Configuration;
